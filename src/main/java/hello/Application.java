359b4175e93ae234de2b98c5f1c9bb8b9bb3c2ac
ntg552000 <ntg552000@gmail.com>
2022-11-03 11:16:07 +0700

    test3

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World test 1";
+        return "Hello Docker World test 3";
     }
 
     public static void main(String[] args) {
